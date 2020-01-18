551e605c06cd4eafe2ec36f89a91b0ee39f756e1
kIMwofe <245902043@qq.com>
2020-01-18 21:30:44 +0800

    优化

--- src/components/Raffle.vue
+++ src/components/Raffle.vue
@@ -92,7 +92,7 @@ export default {
         if (this.drawCode > 0 && !this.showNum) {
           this.showNum = true;
           setTimeout(() => {
-            this.toOrderNum(this.drawCode); // 这里输入数字即可调用
+            this.toOrderNum(Number(this.drawCode)); // 这里输入数字即可调用
             this.$emit("showMsg");
           }, 100);
         }
